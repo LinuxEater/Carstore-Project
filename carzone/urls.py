dc528e0cff27798b38c6f5aa9c85362fb973c44b
LinuxEater <moisessouzasantos001@gmail.com>
2025-07-18 20:17:51 -0300

    fix

--- carzone/urls.py
+++ carzone/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(BASE_DIR, 'carzone', 'static'))
