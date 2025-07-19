e394c0f1ab4a5b97032e76bc12f559ce7b1341c3
LinuxEater <moisessouzasantos001@gmail.com>
2025-07-19 01:25:03 -0300

    feat: customize admin model, add search feature

--- pages/admin.py
+++ pages/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Team
 
 # Register your models here.
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone', 'designation', 'created_at')
+    def thumbnail(self, object):
+        return format_html('<img src="{}" style="width: 50px; height: 50px; border-radius: 50%;">'.format(object.photo.url))
+
+    thumbnail.short_description = 'Photo'
+    
+    list_display = ('id','first_name', 'last_name', 'thumbnail', 'email', 'phone', 'designation', 'created_at')
+    list_display_links = ('id','thumbnail','first_name',)
     search_fields = ('first_name', 'last_name', 'email')
     list_filter = ('designation',)
 
