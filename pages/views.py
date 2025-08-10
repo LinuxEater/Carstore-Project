2c38c2ca67cff383f08aa8207654a19eb8ae2c52
LinuxEater <moisessouzasantos001@gmail.com>
2025-08-10 13:23:22 -0300

    feat: add featured cars

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 from datetime import datetime
 from .models import Team
 
 # Create your views here.
 
 def home(request):
+    featured_cars = Car.objects.filter(is_featured=True)
     now = datetime.now()
     teams = Team.objects.all()
     day_of_week = now.strftime('%A') 
@@ -12,6 +14,7 @@ def home(request):
     context = {
         'teams': teams,
         'day_of_week': day_of_week,
+        'featured_cars': featured_cars,
         'hour': hour,
     }
     return render(request, 'pages/home.html', context)
